77c2adb03445d8668b7461e026e48e2bc0872c4b
ezrahdermessi <ezrahdermessi@gmail.com>
2026-04-30 19:51:42 +0300

    Add reset_admin command to auto-fix password on deploy

---
+++ stayup_furniture/management/commands/reset_admin.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+import os
+
+class Command(BaseCommand):
+    help = 'Create or reset admin user'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = "admin"
+        email = "admin@stayupnation.com"
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "Admin123!")
+
+        if User.objects.filter(username=username).exists():
+            user = User.objects.get(username=username)
+            user.set_password(password)
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f"Password reset for {username}"))
+        else:
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS(f"Superuser {username} created"))
